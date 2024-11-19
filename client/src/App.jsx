aaeaa81a688e635b701306898ae9e2f81c177351
thankuxari <konstantinoseyosmos@gmail.com>
2024-11-19 16:09:17 +0200

    Velona beta 0.0.1.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,45 +15,45 @@ axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:5000';
 
 function App() {
-	const user = useRecoilValue(userAtom);
+    const user = useRecoilValue(userAtom);
 
-	const { pathname } = useLocation();
-	return (
-		<div
-			className={`mx-auto w-[72.5%] h-screen ${
-				pathname === '/' ? 'md:w-[790px]' : 'md:w-[650px]'
-			}`}
-		>
-			<Header />
-			<Routes>
-				<Route
-					path="/"
-					element={user ? <Home /> : <Navigate to="/auth" />}
-				/>
-				<Route
-					path="/auth"
-					element={!user ? <Auth /> : <Navigate to="/" />}
-				/>
-				<Route
-					path="/update"
-					element={user ? <UpdateProfile /> : <Navigate to="/auth" />}
-				/>
-				<Route path="/auth" element={<Auth />} />
-				<Route path="/" element={<Home />} />
-				<Route
-					path="/:username"
-					element={
-						user ? <UserProfilePage /> : <Navigate to="/auth" />
-					}
-				/>
-				<Route
-					path="/:username/:postid"
-					element={user ? <PostPage /> : <Navigate to="/auth" />}
-				/>
-				<Route path="/patchnotes" element={<Patchnotes />} />
-			</Routes>
-		</div>
-	);
+    const { pathname } = useLocation();
+    return (
+        <div
+            className={`mx-auto w-[72.5%] h-screen ${
+                pathname === '/' ? 'md:w-[790px]' : 'md:w-[650px]'
+            }`}
+        >
+            <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={user ? <Home /> : <Navigate to="/auth" />}
+                />
+                <Route
+                    path="/auth"
+                    element={!user ? <Auth /> : <Navigate to="/" />}
+                />
+                <Route
+                    path="/update"
+                    element={user ? <UpdateProfile /> : <Navigate to="/auth" />}
+                />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/" element={<Home />} />
+                <Route
+                    path="/:username"
+                    element={
+                        user ? <UserProfilePage /> : <Navigate to="/auth" />
+                    }
+                />
+                <Route
+                    path="/:username/:postid"
+                    element={user ? <PostPage /> : <Navigate to="/auth" />}
+                />
+                <Route path="/patchnotes" element={<Patchnotes />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,57 +5,57 @@ import { useRecoilValue } from 'recoil';
 import { CiUser } from 'react-icons/ci';
 
 function Header() {
-	const user = useRecoilValue(userAtom);
+    const user = useRecoilValue(userAtom);
 
-	return (
-		<header>
-			<div className="navbar bg-base-100  py-7">
-				<div className="flex-auto">
-					<Link
-						to={'/patchnotes'}
-						className="btn btn-ghost text-xl font-semibold"
-					>
-						Velona <span className="badge badge-primary">Beta</span>
-					</Link>
-				</div>
-				<div className="flex-auto">
-					<Link to={`/`}>
-						<img
-							src="/public/images/logo.gif"
-							className="size-8"
-							alt=""
-						/>{' '}
-					</Link>
-				</div>
-				<div className="flex-1  gap-3">
-					<div className="dropdown dropdown-end">
-						<div
-							tabIndex={0}
-							role="button"
-							className="btn btn-ghost btn-circle avatar"
-						>
-							<div className="rounded-full">
-								{user && (
-									<Link to={`/${user.user?.username}`}>
-										<img
-											className="size-24 rounded-full"
-											src={
-												user.user.profilePic
-													? `http://localhost:5000/${user.user?.profilePic}`
-													: './public/images/no_user_profile_pic.jpg'
-											}
-											alt="User Profile Image"
-										/>
-									</Link>
-								)}
-							</div>
-						</div>
-					</div>
-					{user && <LogoutButton />}
-				</div>
-			</div>
-		</header>
-	);
+    return (
+        <header>
+            <div className="navbar bg-base-100  py-7">
+                <div className="flex-auto">
+                    <Link
+                        to={'/patchnotes'}
+                        className="btn btn-ghost text-xl font-semibold"
+                    >
+                        Velona <span className="badge badge-primary">Beta</span>
+                    </Link>
+                </div>
+                <div className="flex-auto">
+                    <Link to={`/`}>
+                        <img
+                            src="/public/images/logo.gif"
+                            className="size-8"
+                            alt=""
+                        />{' '}
+                    </Link>
+                </div>
+                <div className="flex-1  gap-3">
+                    <div className="dropdown dropdown-end">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost btn-circle avatar"
+                        >
+                            <div className="rounded-full">
+                                {user && (
+                                    <Link to={`/${user.user?.username}`}>
+                                        <img
+                                            className="size-24 rounded-full"
+                                            src={
+                                                user.user.profilePic
+                                                    ? `http://localhost:5000/${user.user?.profilePic}`
+                                                    : './public/images/no_user_profile_pic.jpg'
+                                            }
+                                            alt="User Profile Image"
+                                        />
+                                    </Link>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    {user && <LogoutButton />}
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
